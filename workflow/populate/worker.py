7aac4b3fba3014d0424d13420b48dba033beaa13
Arseny Finkelstein <31078965+arsenyf@users.noreply.github.com>
2023-01-05 16:42:13 +0200

    Update worker.py

--- workflow/populate/worker.py
+++ workflow/populate/worker.py
@@ -33,7 +33,7 @@ standard_worker = DataJointWorker('standard_worker',
 #analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'session': 7}
 #standard_worker(analysis_pop.ROISVDPython)
 
-analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'session': 1}
+analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'session': 2}
 standard_worker(analysis_pop.ROISVDPython)
 
 

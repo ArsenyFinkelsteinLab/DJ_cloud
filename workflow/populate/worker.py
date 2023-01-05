7937f40e2bd253b9c6abac891dc95e4d20f28930
Arseny Finkelstein <31078965+arsenyf@users.noreply.github.com>
2023-01-05 02:02:24 +0200

    Update worker.py

--- workflow/populate/worker.py
+++ workflow/populate/worker.py
@@ -23,8 +23,8 @@ standard_worker = DataJointWorker('standard_worker',
                                   autoclear_error_patterns=autoclear_error_patterns)
 
 # restrict to 1 session
-#analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724'}
-analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'session': 2}
+analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724'}
+#analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'session': 2}
 
 #standard_worker(analysis_pop.ROISVDPython)
 

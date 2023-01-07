cf451c67cdb4619daa7a3970b8f8092044f7b059
lsusman <lee.susman@gmail.com>
2023-01-07 14:18:24 +0200

    Update worker.py

--- workflow/populate/worker.py
+++ workflow/populate/worker.py
@@ -33,12 +33,16 @@ standard_worker = DataJointWorker('standard_worker',
 #analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'session': 7}
 #standard_worker(analysis_pop.ROISVDPython)
 
+
+###### Latest runs of SVD
 #analysis_pop.ROISVDPython.key_source &= {'subject_id': '463190'}
-analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'time_bin': '0'}
-standard_worker(analysis_pop.ROISVDPython)
+#analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'time_bin': '0'}
+#standard_worker(analysis_pop.ROISVDPython)
 
 
-
+#### Shared variance computation
+shared_variance_analysis.SVC.key_source &= {'subject_id': '464724', 'time_bin': '0'}
+standard_worker(shared_variance_analysis.SVC)
 
 
 # example to add new workers  

1d14a527789f5be7ac9b06ef0cdfbb4676c6f1ab
lsusman <lee.susman@gmail.com>
2023-01-16 20:22:37 +0200

    Update analysis_pop_area.py

--- workflow/pipeline/analysis_pop_area.py
+++ workflow/pipeline/analysis_pop_area.py
@@ -16,15 +16,15 @@ stimanal = dj.VirtualModule('stimanal', 'arseny_learning_photostim_anal')
 lab = dj.VirtualModule('lab', 'map_lab')
 
 
-def FetchChunked(relation, idx_name, val_name, chunk_size):
-    idx = relation.fetch(idx_name, order_by=idx_name)
+def FetchChunked(relation_area, relation_tot, idx_name, val_name, chunk_size):
+    idx = relation_tot.fetch(idx_name, order_by=idx_name)
     num_elements = len(idx)
     num_chunks = (num_elements + (chunk_size - 1)) // chunk_size
     parts = []
     for i_chunk in range(num_chunks):
         i = i_chunk * chunk_size + 1
         # Don't need to manually check for the remainder; relation does it
-        rel_part = relation & f"{idx_name} >= {i}" & f"{idx_name} < {i + chunk_size}"
+        rel_part = relation_area & f"{idx_name} >= {i}" & f"{idx_name} < {i + chunk_size}"
         parts.append(np.asarray(rel_part.fetch(val_name, order_by=idx_name)))
     return np.concatenate(parts)
 
@@ -92,15 +92,17 @@ class ROISVDArea(dj.Computed):
         threshold_variance_explained = 0.9
         num_components_save = 100
 
-        rel_data1 = (img.ROIdeltaF*img.ROIBrainArea & key) - img.ROIBad
         self2 = SVDAreaSingularValues
         self3 = SVDAreaTemporalComponents
         for i, time_bin in enumerate(time_bin_vector):
-            self.compute_SVD(self2, self3, key, rel_data1, flag_zscore, time_bin, thresholds_for_event, threshold_variance_explained, num_components_save)
+            self.compute_SVD(self2, self3, key, flag_zscore, time_bin, thresholds_for_event, threshold_variance_explained, num_components_save)
 
-    def compute_SVD(self, self2, self3, key, rel_data1, flag_zscore, time_bin, thresholds_for_event, threshold_variance_explained, num_components_save):
+    def compute_SVD(self, self2, self3, key, flag_zscore, time_bin, thresholds_for_event, threshold_variance_explained, num_components_save):
         rel_FOVEpoch = img.FOVEpoch & key
         rel_FOV = img.FOV & key
+        rel_data_area = (img.ROIdeltaF*img.ROIBrainArea & key) - img.ROIBad
+        rel_data_tot = (img.ROIdeltaF & key) - img.ROIBad
+
         if 'imaging_frame_rate' in rel_FOVEpoch.heading.secondary_attributes:
             imaging_frame_rate = rel_FOVEpoch.fetch1('imaging_frame_rate')
         else:
@@ -109,10 +111,10 @@ class ROISVDArea(dj.Computed):
         # TODO: Use unique_roi_number or something esle to guarantee consistent order
         # (but unique_roi_number is not a primary key)
 
-        if 'dff_trace' in rel_data1.heading.secondary_attributes:
-            F = FetchChunked(rel_data1 & key, 'roi_number', 'dff_trace', 500)
+        if 'dff_trace' in rel_data_area.heading.secondary_attributes:
+            F = FetchChunked(rel_data_area & key, rel_data_tot & key, 'roi_number', 'dff_trace', 500)
         else:
-            F = FetchChunked(rel_data1 & key, 'roi_number', 'spikes_trace', 500)
+            F = FetchChunked(rel_data_area & key, rel_data_tot & key, 'roi_number', 'spikes_trace', 500)
 
         F_binned = np.array([MakeBins(Fi.flatten(), time_bin * imaging_frame_rate) for Fi in F])
 

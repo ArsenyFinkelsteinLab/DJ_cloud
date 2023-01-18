fad349ebf569b8ac8e8ea4a91669e75d95a82961
lsusman <lee.susman@gmail.com>
2023-01-18 20:01:09 +0200

    Update analysis_meso_svd.py

--- workflow/pipeline/analysis_meso_svd.py
+++ workflow/pipeline/analysis_meso_svd.py
@@ -88,7 +88,7 @@ class ROISVDPython(dj.Computed):
 
         flag_zscore = 1
         threshold_variance_explained = 0.9
-        num_components_save = 1000
+        num_components_save = 100
 
         rel_data1 = (img.ROIdeltaF & key) - img.ROIBad
         self2 = SVDSingularValuesPython

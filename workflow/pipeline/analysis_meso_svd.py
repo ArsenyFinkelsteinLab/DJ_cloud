70d4b557139b180a314bdef4747668e56c0e65fd
lsusman <lee.susman@gmail.com>
2023-02-02 01:12:09 +0200

    Update analysis_meso_svd.py

--- workflow/pipeline/analysis_meso_svd.py
+++ workflow/pipeline/analysis_meso_svd.py
@@ -80,7 +80,7 @@ class ROISVD(dj.Computed):
     	# So far the code is only correct for threshold == 0
         thresholds_for_event = [0] # [0, 1, 2]
 
-        time_bin_vector = [0, 1, 1.5]
+        time_bin_vector = [0]#, 1, 1.5]
 
         flag_zscore = 1
         threshold_variance_explained = 0.9

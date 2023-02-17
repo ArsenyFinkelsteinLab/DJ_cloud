343eb83931fba686f65fe18f9c8ee61cd9964988
Drew Yang <31813282+yambottle@users.noreply.github.com>
2023-02-17 11:17:36 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,3 +20,6 @@ sleep 20
 echo "----- Cleanup Docker -----"
 docker compose --env-file=../../.env  -f docker-compose-standard_worker.yaml down
 docker system prune -a -f --volumes
+
+echo "----- Shutdown server -----"
+sudo shutdown now

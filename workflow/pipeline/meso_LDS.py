975fcf44aaee1381f9039e271efe59eea76afbaf
lsusman <lee.susman@gmail.com>
2023-04-27 21:53:01 +0300

    Update meso_LDS.py

--- workflow/pipeline/meso_LDS.py
+++ workflow/pipeline/meso_LDS.py
@@ -38,8 +38,8 @@ class SVDLDS(dj.Computed):
  #           observed_dim_vals = [30]
  #           latent_dim_vals = [20]
  #       else:
-        observed_dim_vals = [100]
-        latent_dim_vals = [80]
+        observed_dim_vals = [80]
+        latent_dim_vals = [40]
 
         for observed_dim in observed_dim_vals:
             for latent_dim in latent_dim_vals:
